3b55f4912d7050551c30dff9b2bb12f59bf99a02
Lyric Doshi <lyric@mit.edu>
2022-11-29 21:46:56 -0800

    Refresh tokens daily.

--- heart_of_gold_main.py
+++ heart_of_gold_main.py
@@ -1,11 +1,13 @@
 """Run the ship."""
 
-from typing import List
+from typing import Callable, List
 
 import collections
 import datetime
+import functools
 import spotipy
 import pynput
+import threading
 import time
 import wyze_sdk
 
@@ -73,6 +75,31 @@ _LIGHT_CONTROLS_LIVING_ROOM_NICKNAME_PREFIX = "living_room"
 _LIGHT_CONTROLS_EMAIL = "ENTER HERE"
 _LIGHT_CONTROLS_PASSWORD = "ENTER HERE"
 
+_SECONDS_IN_A_DAY = 60 * 60 * 24
+
+
+class DailyTaskManager:
+    def __init__(self):
+        self._tasks = []
+        self._timer = None
+        self._is_running = False
+
+    def _run_tasks(self) -> None:
+        for task in self._tasks:
+            task()
+
+        self._is_running = False
+        self.start()
+
+    def start(self) -> None:
+        if not self._is_running:
+            self._timer = threading.Timer(_SECONDS_IN_A_DAY, self._run_tasks)
+            self._timer.start()
+            self._is_running = True
+
+    def add_task(self, task: Callable) -> None:
+        self._tasks.append(task)
+
 
 def _beep() -> None:
     for _ in range(5):
@@ -110,9 +137,20 @@ def _create_radio_plugin() -> plugin_api.Plugin:
     )
 
 
-def _create_light_controls_plugins() -> List[plugin_api.Plugin]:
+def _refresh_wyze_client_token(wyze_client: wyze_sdk.Client) -> None:
+    response = wyze_client.refresh_token()
+    wyze_client._token = response["data"]["access_token"]
+    wyze_client._refresh_token = response["data"]["refresh_token"]
+
+
+def _create_light_controls_plugins(
+    daily_task_manager: DailyTaskManager,
+) -> List[plugin_api.Plugin]:
     wyze_client = wyze_sdk.Client(
         email=_LIGHT_CONTROLS_EMAIL, password=_LIGHT_CONTROLS_PASSWORD
+    )
+    daily_task_manager.add_task(
+        functools.partial(_refresh_wyze_client_token, wyze_client)
     )
     return [
         light_controls.LightControls(
@@ -167,10 +205,12 @@ class Bridge:
 
 def main():
     bridge = Bridge(reset_code=_RESET_CODE)
+    daily_task_manager = DailyTaskManager()
     bridge.register(_create_radio_plugin())
-    for light_control_plugin in _create_light_controls_plugins():
+    for light_control_plugin in _create_light_controls_plugins(daily_task_manager):
         bridge.register(light_control_plugin)
 
+    daily_task_manager.start()
     with pynput.keyboard.Listener(on_release=bridge.command) as listener:
         listener.join()
 
